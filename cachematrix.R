cf29841df13ff2d7c892ea3fd0d409e8224071c2
prabhushrikant <shrikant@Shrikants-MacBook-Pro.local>
2015-10-25 03:00:54 -0700

    completed programming assignement 2
    Wrote function to test if matrix inverse is cached the first time and
    later it’s getting fetched from the cache.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,36 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#This creates list of functions required to get chached matrix
+#inverse passed in as argument.
+makeCacheMatrix <- function(x = matrix()){
+    
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverseOfX) m <<- inverseOfX
+    getInverse <- function() m
+    list(set = set, get = get,
+    setInverse = setInverse,
+    getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
-
+#This function gets the cached inverse value of the passed in matrix
+#if found in the cache otherwise caches it for later use.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse();
+        if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+        }
+        matrixData <- x$get()
+        m <- solve(matrixData, ...)
+        x$setInverse(m)
+        m
 }
